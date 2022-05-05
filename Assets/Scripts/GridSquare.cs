382cec62652eb6d65dbea307cf0fdeeaf1bbf073
[Ivan Filippov] <ifilippov2020@gmail.com>
2022-05-05 14:32:09 +0300

    First commit

--- Assets/Scripts/GridSquare.cs
+++ Assets/Scripts/GridSquare.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static AlphabetData;
 
 public class GridSquare : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int SquareIndex { get; set; }
+
+    private LetterData _normalLetterData;
+    private LetterData _selectedLetterData;
+    private LetterData _correctLetterData;
+
+    private SpriteRenderer _displayedSprite;
+
     void Start()
     {
-        
+        _displayedSprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetSprite
+        (
+        LetterData normalLetterData, 
+        LetterData selectedLetterData, 
+        LetterData correctLetterData
+        )
+    { 
+        _normalLetterData = normalLetterData;
+        _selectedLetterData = selectedLetterData;
+        _correctLetterData = correctLetterData;
+
+        //_displayedSprite.sprite = _normalLetterData.Sprite; //
+        GetComponent<SpriteRenderer>().sprite = _normalLetterData.Sprite;
     }
 }
--- Assets/Scripts/WordsGrid.cs
+++ Assets/Scripts/WordsGrid.cs
@@ -11,19 +11,115 @@ public class WordsGrid : MonoBehaviour
     public float squareOffset = 0.0f;
     public float topPosition;
 
+    [SerializeField] private Vector3 _defaultScale = new Vector3(1.5f, 1.5f, 1f);
     private List<GameObject> _squareList = new List<GameObject>();
     private Camera _camera;
+    
 
     void Start()
     {
         _camera = Camera.main;
+        SpawnGridSquares();
+        SetSquaresPosition();
+    }
+
+    private void SetSquaresPosition()
+    {
+        Rect squareRect = _squareList[0].GetComponent<SpriteRenderer>().sprite.rect;
+        Transform squareTransform = _squareList[0].GetComponent<Transform>();
+        Vector2 offset = new Vector2
+        {
+            x = (squareRect.width * squareTransform.localScale.x + squareOffset) * 0.01f,
+            y = (squareRect.height * squareTransform.localScale.y + squareOffset) * 0.01f
+        };
+
+        var startPosition = GetFirstSquarePosition();
+        int columnNumber = 0;
+        int rowNumber = 0;
+
+        foreach (var square in _squareList)
+        {
+            if (rowNumber + 1 > currentGameData.selectedBoardData.Rows)
+            {
+                columnNumber++;
+                rowNumber = 0;
+            }
+            var positionX = startPosition.x + offset.x * columnNumber;
+            var positionY = startPosition.y - offset.y * rowNumber;
+
+            square.GetComponent<Transform>().position = new Vector2(positionX, positionY);
+            rowNumber++;
+        }
+    }
+
+    private Vector2 GetFirstSquarePosition()
+    {
+        var startPosition = new Vector2(0f, transform.position.y);
+        var squareRect = _squareList[0].GetComponent<SpriteRenderer>().sprite.rect;
+        var squareTransform = _squareList[0].GetComponent<Transform>();
+        var squareSize = new Vector2(0f, 0f);
+
+        squareSize.x = squareRect.width * squareTransform.localScale.x;
+        squareSize.y = squareRect.height * squareTransform.localScale.y;
+
+        var midWidthPosition = (((currentGameData.selectedBoardData.Columns - 1) * squareSize.x) / 2) * 0.01f;
+        float midWidthHeight = (((currentGameData.selectedBoardData.Rows - 1) * squareSize.y) / 2) * 0.01f;
+
+        startPosition.x = (midWidthPosition != 0) ? midWidthPosition * -1 : midWidthPosition;
+        startPosition.y += midWidthHeight;
+
+        return startPosition;
     }
 
     private void SpawnGridSquares()
     {
         if (currentGameData != null)
-        { 
-        
+        {
+            var squareScale = GetSquareScale(_defaultScale);
+            foreach (var squares in currentGameData.selectedBoardData.Board)
+            {
+                foreach (var squareLetter in squares.Row)
+                {
+                    var normalLetterData = alphabetData.AlphabetNormal.Find(data => data.Letter == squareLetter);
+                    var selectedLetterData = alphabetData.AlphabetHighlighted.Find(data => data.Letter == squareLetter);
+                    var correctLetterData = alphabetData.AlphabetWrong.Find(data => data.Letter == squareLetter);
+
+                    if (normalLetterData.Sprite == null || selectedLetterData.Sprite == null)
+                    {
+                        Debug.LogError
+                            ("All fields in your array should have some letters. Press \"Fill Up With Random\" in your board data to add random letter. Letter: " + squareLetter);
+#if UNITY_EDITOR
+                        if (UnityEditor.EditorApplication.isPlaying)
+                        {
+                            UnityEditor.EditorApplication.isPlaying = false;
+                        }
+#endif
+                    }
+                    else 
+                    {
+                        var square = Instantiate(gridSquarePrefab);
+                        square.GetComponent<GridSquare>().SetSprite(normalLetterData, selectedLetterData, correctLetterData);
+                        var squareTransform = square.GetComponent<Transform>();
+                        squareTransform.SetParent(this.transform);
+                        squareTransform.position = Vector3.zero;
+                        squareTransform.localScale = squareScale;
+                        _squareList.Add(square);
+
+                        //var square = Instantiate(gridSquarePrefab);
+                        //square.GetComponent<GridSquare>().SetSprite(normalLetterData, selectedLetterData, correctLetterData);
+                        //square.transform.SetParent(this.transform);
+                        //square.transform.position = new Vector3(0f, 0f, 0f);
+                        //square.transform.localScale = squareScale;
+                        //_squareList.Add(square);
+
+                        //_squareList.Add(Instantiate(gridSquarePrefab));
+                        //_squareList[_squareList.Count - 1].GetComponent<GridSquare>().SetSprite(normalLetterData, selectedLetterData, correctLetterData);
+                        //_squareList[_squareList.Count - 1].transform.SetParent(this.transform);
+                        //_squareList[_squareList.Count - 1].GetComponent<Transform>().position = new Vector3(0f, 0f, 0f);
+                        //_squareList[_squareList.Count - 1].transform.localScale = squareScale;
+                    }
+                }
+            }
         }
     }
 
