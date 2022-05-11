b775d69d0144072d7484a2b6e801052dee2ee4ee
[Ivan Filippov] <ifilippov2020@gmail.com>
2022-05-11 18:19:11 +0300

    Work on in menu interactions

---
+++ Assets/Scripts/ScriptableObjects/GameLevelData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "GameLevelData", menuName = "Data/GameLevelData")]
+public class GameLevelData : ScriptableObject
+{
+
+}
