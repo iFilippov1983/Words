efc222c65ded4e8baa3e80822cfaba18e0076eae
[Ivan Filippov] <ifilippov2020@gmail.com>
2022-05-12 16:49:30 +0300

    Level completion logic added + Odin inspector

---
+++ Assets/Scripts/WinPopup.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WinPopup : MonoBehaviour
+{
+    public GameObject winPopup;
+
+    void Start()
+    {
+        winPopup.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnBoardComleted += ShowWinPoppup;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnBoardComleted -= ShowWinPoppup;
+    }
+
+    private async void ShowWinPoppup()
+    {
+        await Task.Delay(1000);
+        winPopup.SetActive(true);
+    }
+
+    public void LoadNextLevel()
+    {
+        GameEvents.LoadNextLevelMethod();
+    }
+}
